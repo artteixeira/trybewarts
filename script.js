dec9b3010eba568e79a184cbe9d65c6250645a9e
Arthur Teixeira <arthursony1@hotmail.com>
2023-02-15 16:17:38 -0300

    parte 3 finalizada

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const emailLogin = document.querySelector('.login-email');
-const senhaLogin = document.querySelector('.login-password')
+const buttonLogin = document.querySelector('#login-button');
+const emailLogin = document.querySelector('#login-email');
+const senhaLogin = document.querySelector('#login-password');
 
-if (emailLogin.value === tryber@teste.com && )
+buttonLogin.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (emailLogin.value === 'tryber@teste.com' && senhaLogin.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+});
